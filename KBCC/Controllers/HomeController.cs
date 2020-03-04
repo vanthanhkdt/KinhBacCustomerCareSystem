a1acebac49ab2a89591d1f4e61e5dc9ec4ac2afb
vanthanhkdt <vanthanhkdt@users.noreply.github.com>
2020-03-04 12:28:13 +0700

    AddExcelImport

--- KBCC/Controllers/HomeController.cs
+++ KBCC/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using KBCC.Models;
+using OfficeOpenXml;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -26,5 +29,83 @@ namespace KBCC.Controllers
 
             return View();
         }
+
+        public JsonResult ImportExcel()
+        {
+            List<Order> excelData = new List<Order>();
+            if (Request.Files.Count > 0)
+            {
+                try
+                {
+                    HttpFileCollectionBase files = Request.Files;
+                    for (int i = 0; i < files.Count; i++)
+                    {
+                        HttpPostedFileBase file = files[i];
+                        string fname;
+                        if (Request.Browser.Browser.ToUpper() == "IE" || Request.Browser.Browser.ToUpper() == "INTERNETEXPLORER")
+                        {
+                            string[] testfiles = file.FileName.Split(new char[] { '\\' });
+                            fname = testfiles[testfiles.Length - 1];
+                        }
+                        else
+                        {
+                            fname = file.FileName;
+                        }
+                        var newName = fname.Split('.');
+                        fname = newName[0] + "_" + DateTime.Now.Ticks.ToString() + "." + newName[1];
+                        var uploadRootFolderInput = AppDomain.CurrentDomain.BaseDirectory + "Uploaded\\Excel";
+                        Directory.CreateDirectory(uploadRootFolderInput);
+                        var directoryFullPathInput = uploadRootFolderInput;
+                        fname = Path.Combine(directoryFullPathInput, fname);
+                        file.SaveAs(fname);
+                        string xlsFile = fname;
+                        excelData = ReadExcel(fname);
+                    }
+                    if (excelData.Count > 0)
+                    {
+                        return Json(excelData, JsonRequestBehavior.AllowGet);
+                    }
+                    else
+                        return Json(false, JsonRequestBehavior.AllowGet);
+                }
+                catch (Exception ex)
+                {
+                    return Json(false, JsonRequestBehavior.AllowGet);
+                }
+            }
+            else
+            {
+                return Json(false, JsonRequestBehavior.AllowGet);
+            }
+        }
+        public List<Order> ReadExcel(string FilePath)
+        {
+            try
+            {
+                List<Order> excelData = new List<Order>();
+                FileInfo existingFile = new FileInfo(FilePath);
+                using (ExcelPackage package = new ExcelPackage(existingFile))
+                {
+                    ExcelWorksheet worksheet = package.Workbook.Worksheets[1];
+                    int rowCount = worksheet.Dimension.End.Row;
+                    for (int row = 2; row <= rowCount; row++)
+                    {
+                        excelData.Add(new Order()
+                        {
+                            CustomerPhone = worksheet.Cells[row, 1].Value.ToString().Trim(),
+                            CustomerName = worksheet.Cells[row, 2].Value.ToString().Trim(),
+                            CustomerAddress = worksheet.Cells[row, 3].Value.ToString().Trim(),
+                            InternalNote = worksheet.Cells[row, 4].Value.ToString().Trim(),
+                        });
+                    }
+                }
+                return excelData;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
     }
 }
---
+++ KBCC/Scripts/common/toast.js
@@ -0,0 +1,100 @@
+﻿function toast($btn, title, message, showDuration, hideDuration,
+    timeOut, extendedTimeOut, showEasing, hideEasing, showMethod, hideMethod,
+    toastIndex, closeButton, debug, progressBar, positionClass, addBehaviorOnToastClick, onclick) {
+    
+    var getMessage = function () {
+        var msg = 'Hi, welcome to Inspinia. This is example of Toastr notification box.';
+        return msg;
+    };
+
+    $('#showsimple').click(function () {
+        // Display a success toast, with a title
+        toastr.success('Without any options', 'Simple notification!')
+    });
+    $('#showtoast').click(function () {
+        var shortCutFunction = $("#toastTypeGroup input:radio:checked").val();
+        var msg = message;
+        var title = title || '';
+        var $showDuration = showDuration;
+        var $hideDuration = hideDuration;
+        var $timeOut = timeOut;
+        var $extendedTimeOut = extendedTimeOut;
+        var $showEasing = showEasing;
+        var $hideEasing = hideEasing;
+        var $showMethod = showMethod;
+        var $hideMethod = hideMethod;
+        var toastIndex = toastCount++;
+        toastr.options = {
+            closeButton: closeButton,
+            debug: debug,
+            progressBar: progressBar,
+            positionClass: positionClass || 'toast-bottom-right',
+            onclick: null
+        };
+        if (addBehaviorOnToastClick) {
+            toastr.options.onclick = function () {
+                alert('You can perform some custom action after a toast goes away');
+            };
+        }
+        if ($showDuration.val().length) {
+            toastr.options.showDuration = $showDuration.val();
+        }
+        if ($hideDuration.val().length) {
+            toastr.options.hideDuration = $hideDuration.val();
+        }
+        if ($timeOut.val().length) {
+            toastr.options.timeOut = $timeOut.val();
+        }
+        if ($extendedTimeOut.val().length) {
+            toastr.options.extendedTimeOut = $extendedTimeOut.val();
+        }
+        if ($showEasing.val().length) {
+            toastr.options.showEasing = $showEasing.val();
+        }
+        if ($hideEasing.val().length) {
+            toastr.options.hideEasing = $hideEasing.val();
+        }
+        if ($showMethod.val().length) {
+            toastr.options.showMethod = $showMethod.val();
+        }
+        if ($hideMethod.val().length) {
+            toastr.options.hideMethod = $hideMethod.val();
+        }
+        if (!msg) {
+            msg = getMessage();
+        }
+        $("#toastrOptions").text("Command: toastr["
+            + shortCutFunction
+            + "](\""
+            + msg
+            + (title ? "\", \"" + title : '')
+            + "\")\n\ntoastr.options = "
+            + JSON.stringify(toastr.options, null, 2)
+        );
+        var $toast = toastr[shortCutFunction](msg, title); // Wire up an event handler to a button in the toast, if it exists
+        $toastlast = $toast;
+        if ($toast.find('#okBtn').length) {
+            $toast.delegate('#okBtn', 'click', function () {
+                alert('you clicked me. i was toast #' + toastIndex + '. goodbye!');
+                $toast.remove();
+            });
+        }
+        if ($toast.find('#surpriseBtn').length) {
+            $toast.delegate('#surpriseBtn', 'click', function () {
+                alert('Surprise! you clicked me. i was toast #' + toastIndex + '. You could perform an action here.');
+            });
+        }
+    });
+}
+var i = -1;
+var toastCount = 0;
+var $toastlast;
+function getLastToast() {
+    return $toastlast;
+}
+$('#clearlasttoast').click(function () {
+    toastr.clear(getLastToast());
+});
+$('#cleartoasts').click(function () {
+    toastr.clear();
+});
