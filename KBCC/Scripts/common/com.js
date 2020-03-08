507961db9933ac736cae28ce73808dfefb29f38c
vanthanhkdt <vanthanhkdt@users.noreply.github.com>
2020-03-08 23:22:07 +0700

    Fix clone issue with event by using .clone(true,true)

---
+++ KBCC/Scripts/common/com.js
@@ -0,0 +1,27 @@
+﻿(function ($) {
+    $.normal = function () {
+    };
+
+    $.fn.sAlert = function (callback) {
+        swal({
+            title: "Bạn có chắc chắn muốn thêm Tag này không?",
+            text: "Tag mới sẽ được thêm vào hệ thống này!",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Có, thêm ngay!",
+            cancelButtonText: "Không, hủy bỏ!",
+            closeOnConfirm: false,
+            closeOnCancel: false
+        },
+            function (isConfirm) {
+                if (isConfirm) {
+                    callback();
+                    //toastr.success("Đã thêm thành công", "Thành công");
+                    //swal("Thành công!", "Đã thêm thành công.", "success");
+                } else {
+                    swal("Đã hủy", "Không có Tag mới nào được thêm.", "error");
+                }
+            });
+    }
+}($));
